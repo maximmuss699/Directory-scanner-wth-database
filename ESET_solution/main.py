f86055dbee0790b94713f1b1a23fc07c7d89af74
Max Samusevich <maxsamumax@gmail.com>
2026-08-12 14:45:51 +0200

    feat: add file size info

--- ESET_solution/main.py
+++ ESET_solution/main.py
@@ -1,6 +1,17 @@
 import os
 
 
-with os.scandir(".") as directory:
-    for entry in directory:
-        print(entry.name)
+def list_files_in_directory():
+    files_info = []
+    for file_name in os.listdir():
+        if os.path.isfile(file_name):
+            file_info = {
+                "name": file_name,
+                "size": os.path.getsize(file_name)
+            }
+            files_info.append(file_info)
+    print(files_info)
+    return files_info
+
+# Running the function
+list_files_in_directory()
